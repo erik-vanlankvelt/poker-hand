93d9c13e3b8d67ade5d621c4d0330bc732f0d82f
System Administrator <root@Jessicas-MacBook-Pro.local>
2015-11-05 14:55:27 -0700

    Finishing up functions

--- pokerHand.js
+++ pokerHand.js
@@ -11,42 +11,42 @@
 
 // A new hand should be created each time before invoking other functions on the hand
 var PokerHand = function( cards ) {
-	'use strict';
+    'use strict';
 
-	this.cards = cards;
+    this.cards = cards;
 };
 
 // Mapping for card suits
 PokerHand.prototype.cardSuitToInt = {
-	's': 0, 
-	'h': 1, 
-	'c': 2, 
-	'd': 3
+    's': 0, 
+    'h': 1, 
+    'c': 2, 
+    'd': 3
 };
 
 // Mapping for card types
 PokerHand.prototype.cardTypeToInt = {
-	'2': 0,
-	'3': 1,
-	'4': 2,
-	'5': 3,
-	'6': 4,
-	'7': 5,
-	'8': 6,
-	'9': 7,
-	'10': 8,
-	'J': 9,
-	'Q': 10,
-	'K': 11,
-	'A': 12
+    '2': 0,
+    '3': 1,
+    '4': 2,
+    '5': 3,
+    '6': 4,
+    '7': 5,
+    '8': 6,
+    '9': 7,
+    '10': 8,
+    'J': 9,
+    'Q': 10,
+    'K': 11,
+    'A': 12
 };
 
 // Mapping for poker hand types and their respective rank
 PokerHand.prototype.typeToRank = {
-	'HIGH_CARD': 'worst', 
-	'PAIR': '8th', 
-	'TWO_PAIR': '7th', 
-	'TRIPS': '6th',
+    'HIGH_CARD': 'worst', 
+    'PAIR': '8th', 
+    'TWO_PAIR': '7th', 
+    'TRIPS': '6th',
     'STRAIGHT': '5th', 
     'FLUSH': '4th', 
     'FULL_HOUSE': '3rd', 
@@ -110,38 +110,69 @@ PokerHand.prototype.getRank = function() {
     return this.typeToRank[ this.getType() ];
 };
 
+// Determines the number of cards with the same type, 
+// and returns an array
+// PAIR, TWO_PAIR', TRIPS, FULL_HOUSE, QUADS
+PokerHand.prototype.getSameType = function() {
+    'use strict';
+
+    // 13 possible card types per suit
+    var types = new Array(13);
+
+    // Set each value to zero
+    for ( var i = 0; i < 13; i++ ) {
+        types[i] = 0;                
+    }
+
+    for ( var i = 0; i < this.cards.length; i++ ) {
+        var thisType = this.getCardType( this.cards[i] );
+
+        // Increment type at key
+        types[ this.cardTypeToInt[ thisType ] ] += 1;
+    };
+
+    return types;
+};
+
 // Determine the type of poker hand
 PokerHand.prototype.getType = function() {
     'use strict';
 
-    var type = 'HIGH_CARD';
+    var totalCards = 5,
+        isFlush = this.numSameSuit() === totalCards,
+        isStraight = this.numInOrder() === totalCards,
+        pairCount = 0,
+        sameTypeArray = this.getSameType(),
+        type = 'HIGH_CARD';
 
-    if ( ) {
+    // Special case to check for number of pairs
+    for ( var i = 0; i < sameTypeArray.length; i++ ) {
 
-    } else if ( ) {
-
-    } else if ( ) {
-
-    } else if ( ) {
-
-    } else if ( ) {
-
-    } else if ( ) {
-
-    } else if ( ) {
-
-    } else if ( ) {
-
+        if ( sameTypeArray[i] === 2 ) {
+            pairCount++;
+        }
     }
 
-	type = 'PAIR';
-	type = 'TWO_PAIR';
-	type = 'TRIPS';
-    type = 'STRAIGHT'; 
-    type = 'FLUSH';
-    type = 'FULL_HOUSE';
-    type = 'QUADS';
-    type = 'STRAIGHT_FLUSH';
+    if ( isFlush && isStraight ) {
+        type = 'STRAIGHT_FLUSH';
+    } else if ( isFlush ) {
+        type = 'FLUSH';
+    } else if ( isStraight ) {
+        type = 'STRAIGHT';
+    } else if ( sameTypeArray.indexOf( 4 ) > -1 ) {
+        // contains 4 of the same type
+        type = 'QUADS';
+    } else if ( sameTypeArray.indexOf( 3 ) > -1 && pairCount ) {
+        // contains 3 of the same type and 2 of the same type
+        type = 'FULL_HOUSE';
+    } else if ( sameTypeArray.indexOf( 3 ) > -1 ) {
+        // contains 3 of the same type
+        type = 'TRIPS';
+    } else if ( pairCount > 1 ) {
+        type = 'TWO_PAIR';
+    } else if ( pairCount ) {
+        type = 'PAIR';
+    }
 
     return type;
 };
@@ -151,7 +182,31 @@ PokerHand.prototype.getType = function() {
 PokerHand.prototype.numInOrder = function() {
     'use strict';
 
-    var numInOrder;
+    var numInOrder = 0,
+        typeArray = [];
+
+    // Convert the type strings to integers and push to an array
+    for ( var i = 0; i < this.cards.length; i++ ) {
+        var thisType = this.getCardType( this.cards[i] );
+
+        typeArray.push( this.cardTypeToInt[ thisType ] );
+    };
+
+    // Sort the array by number
+    typeArray = typeArray.sort( function( a, b ) { 
+        return a - b;
+    });
+
+    // Find the number of cards in order
+    for ( var i = 0; i < typeArray.length; i++ ) {
+        var thisCard = typeArray[i],
+            nextCard = typeArray[i + 1];
+        
+        // Are the two cards within one?
+        if ( thisCard === nextCard + 1  ) {
+            numInOrder += 1;
+        }
+    };
 
     return numInOrder;
 };
@@ -161,23 +216,22 @@ PokerHand.prototype.numInOrder = function() {
 PokerHand.prototype.numSameSuit = function() {
     'use strict';
 
-    var suits = [0, 0, 0, 0];
+    // 4 possible suits
+    var suits = new Array(4);
+
+    // Set each value to zero
+    for ( var i = 0; i < 4; i++ ) {
+        suits[i] = 0;                 
+    }
 
     for ( var i = 0; i < this.cards.length; i++ ) {
-    	var thisSuit = this.getCardSuit( this.cards[i] );
+        var thisSuit = this.getCardSuit( this.cards[i] );
 
-        suits[ this.cardSuitToInt[ thisSuit ] ] += 1;
+        // Increment suit at key
+        suits[ this.cardTypeToInt[ thisSuit ] ] += 1;
     };
 
     return Math.max.apply( null, suits );
-};
-
-// Determines the number of cards with the same type
-// PAIR, TWO_PAIR', TRIPS, FULL_HOUSE, QUADS
-PokerHand.prototype.numSameType = function() {
-    var numSameType;
-
-    return numSameType;
 };
 
 module.exports = PokerHand;
